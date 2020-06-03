b2c9620103ec9a99325f2813378ac0f808697160
Robin Dhull <robin707singh@gmail.com>
2020-06-04 01:11:37 +0530

    Update Renaming Files.py

--- Renaming Files.py
+++ Renaming Files.py
@@ -8,9 +8,6 @@ file = open("path/youtubePlaylist.txt", 'r')
 for line in file:
     if line.strip():
         lst.append(line)
-s = ''
-for i in range(len(lst)):
-    lst[i] = lst[i].strip()
 
 i = 0
 for filename in os.listdir(path):
